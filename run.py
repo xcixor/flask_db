cf071513872bf537f3fe9ccd6112503fc42c6814
Peter <pndungu54@gmail.com>
2017-10-12 19:34:55 +0300

    added the migration script an initid the db

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ import os
 
 from flask_script import Shell, Manager
 
+from flask_migrate import Migrate, MigrateCommand
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -19,6 +21,9 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 db = SQLAlchemy(app)
+#eeps track of changes to a database schema, and then incremental changes
+#can be applied to the database
+migrate = Migrate(app, db)
 
 
 
@@ -42,10 +47,21 @@ class ShoppingList(db.Model):
 
     def __repr__(self):
         return "[Id: {0.id}, Name: {0.name}, Owner: {0.user_id}]".format(self) 
+class ShoppingListItem(db.Model):
+    """Defines a shopping list item"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    quantity =db.Column(db.Integer, default=1)
+    price = db.Column(db.Integer, default=100)
+
+    def __repr__(self):
+        return "Id: {0.id}, Name: {0.name}, Quantity: {0.quantity}, Price: {0.price}".format(self)
+
 def make_shell_context():
     return dict(app=app, db=db, User=User, ShoppingList=ShoppingList)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
