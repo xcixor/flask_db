d1cdc1f983cdd848287e4fbbbda82b4aaebb767c
Peter <pndungu54@gmail.com>
2017-10-12 12:33:38 +0300

    configured sqlite

--- run.py
+++ run.py
@@ -9,4 +9,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config(['SQLACHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite'))
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
