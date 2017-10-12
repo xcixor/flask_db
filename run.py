86521f935cde5235f4d7d486c76cc7013cf4a2cc
Peter <pndungu54@gmail.com>
2017-10-12 12:29:59 +0300

    added relevant files

---
+++ run.py
@@ -0,0 +1,12 @@
+"""Runs this app"""
+from flask_sqlachemy import SQLAlchemy
+
+from flask import Flask
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config(['SQLACHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite'))
